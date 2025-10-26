17f04625040d281f30cbda584cb67ca5f0fa4290
Ezhok <fedina2005@inbox.ru>
2025-10-27 04:55:52 +0700

    header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+
+const burger = document.querySelector('.burger');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerClose = document.querySelector('.burger-menu__btn');
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    burgerMenu.classList.add('active');
+  });
+}
+
+if (burgerClose) {
+  burgerClose.addEventListener('click', () => {
+    burgerMenu.classList.remove('active');
+  });
+}
+
+// Закрытие меню при клике вне его
+burgerMenu.addEventListener('click', (e) => {
+  if (e.target === burgerMenu) {
+    burgerMenu.classList.remove('active');
+  }
+});
